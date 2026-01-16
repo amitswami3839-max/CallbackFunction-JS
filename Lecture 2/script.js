d2f774defde017b7551118c59d9e2be8f4f7bec6
amitswami3839-max <amitswami3839@gmail.com>
2026-01-16 11:50:28 +0530

    initial commit

---
+++ Lecture 2/script.js
@@ -0,0 +1,32 @@
+// call=demo 
+// function sample(call){
+//     console.log("sample");
+//     call();
+// }
+// function demo(){
+//     console.log("demo")
+// }
+// sample(demo)
+
+// let numbers=[1,2,3]
+// let doubled=numbers.map((x)=>x)
+// console.log(doubled);
+
+// let marks=[10,40,30,20,34,29,0.5,16,8,1]
+
+// let result= marks.filter((mark)=>mark>=16);
+// console.log(result);
+
+// question - take different marks and print the marks and message
+// of pass and fail for example-"pass"
+
+
+// let marks=[10,20,30,22,24,18,17,15,12,14]
+// let result= marks.filter((mark)=>mark>=12)
+// marks.forEach(mark=>{
+//     if (mark >= 12){
+//         console.log(mark + "pass");
+// }else{
+//     console.log(mark + "fail");
+// }
+// });
